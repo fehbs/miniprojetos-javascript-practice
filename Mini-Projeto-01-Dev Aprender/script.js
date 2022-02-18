fb530303854521cee1d945e091b9151929069a11
Fernando HBS <fernandobailao@gmail.com>
2022-02-18 18:29:27 -0300

    Adicionando mini-projeto-javascript-estudo-01.

---
+++ Mini-Projeto-01-Dev Aprender/script.js
@@ -0,0 +1,41 @@
+//(Mini-Projeto 1) - Trocando Valores de Variáveis
+/*
+let a = 'red';
+let b = 'blue';
+
+/*console.log(a, b);
+console.log(b, a); */
+
+/*
+let a = 'darkgrey, darkblue, darkyellow, darkred';
+let b = 'white, black, green, brown';
+let c = 'box of colors';
+//console.log(a, b, c);
+//console.log(b, c, a);
+//console.log(c, a, b);
+console.log(a, c, b);
+console.log(typeof(a, b, c));
+console.log(`A variavel a tem ${a.length} letras, a variavel b tem ${b.length} letras e a variavel c tem ${c.length} letras.`); */
+
+let num0 = Number(2);
+let num1 = Number(4);
+let num2 = Number(3);
+
+console.log(num0, num1, num2);
+console.log(num1, num2, num0);
+console.log(num2, num0, num1);
+
+console.log(typeof[num0, num1, num2]);
+console.log(typeof(num0, num1, num2));
+
+
+
+
+
+
+
+
+
+
+
+
