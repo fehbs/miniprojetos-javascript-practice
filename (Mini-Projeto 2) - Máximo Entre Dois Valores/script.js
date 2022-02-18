bd0be50ba7434161485294b7a86aa8fdf0c66e23
Fernando HBS <fernandobailao@gmail.com>
2022-02-18 18:29:56 -0300

    Adicionando mini-projeto-javascript-estudo-02.

---
+++ (Mini-Projeto 2) - Máximo Entre Dois Valores/script.js
@@ -0,0 +1,114 @@
+ // (Mini-Projeto 2) - Máximo Entre Dois Valores
+//  Escreva uma fução que usa 2 números e retorna o maior entre eles.
+/*
+let valorMaior = max(5,10);  // teste - 0
+console.log(valorMaior);
+
+function max(num1,num2) {
+    if(num1 > num2)
+        return num1;
+    else return num2;
+}
+
+
+let valorMenor = min(5, 10);  // teste - 1
+console.log(valorMenor);
+
+function min(number1, number2) {
+    if(number1 < number2)
+        return number1;
+    else return number2;
+}  */
+
+
+/*
+let valorMaior = max(300, 10.99);  // teste - 2
+
+function max(n1, n2) {
+    if (n1 > n2)
+         return n1;
+    else return n2;
+}
+
+console.log(valorMaior);
+
+
+let valorMinimo = min(10, 7);  // - 3
+
+function min(n1, n2) {
+    if (n1 < n2)
+         return n1;
+    else return n2;     
+}
+
+console.log(valorMinimo); */
+
+// Função Ternária //
+/*
+let valorMaior = max(10, 10);
+ console.log(valorMaior);
+ 
+ function max(numero1, numero2) {
+     return numero1 >= numero2 ? numero1: numero1;
+ }
+
+
+ let valorMenor = min(1000, 70.98);
+
+ function min(num1, num2) {
+     return num1 < num2 ? num1: num2;
+ }
+ console.log(valorMenor); */
+
+ 
+
+ 
+
+
+
+
+
+
+
+
+
+
+// ##Functions##ETUDE## //
+/*let corSite = "blue";
+function resetaCor(){
+    corSite = "white";
+};
+
+console.log(corSite);
+resetaCor();
+console.log(corSite);
+
+//#################//
+
+let fonte = Number(27);
+function altTamanhoFonte(){
+    fonte = 17;
+};
+
+console.log(fonte);
+altTamanhoFonte();
+console.log(fonte);
+
+//#################//
+
+let style = "fantasy";
+function altstylefont(){
+    style = "arial";
+};
+
+console.log(style);
+altstylefont();
+console.log(style); */
+
+//###############//
+
+
+
+
+
+
